4b96a397ccea5c199300a4513bf106546324d225
vilnytskyy <vilnytskyy21@gmail.com>
2021-07-12 02:11:40 -0400

    Added dummy data for campus addresses and student email and gpa

--- database/utils/seedDB.js
+++ database/utils/seedDB.js
@@ -1,22 +1,26 @@
 const { Campus, Student } = require('../models');
 
 const seedDB = async () => {
-	const dummy_campus = await Campus.create({
-		name: "Hunter College",
-		description: "This is a school in NYC."
-	});
-	const dummy_campus2 = await Campus.create({
-		name: "Harvard",
-		description: "This is a school in MA."
-	});
+    const dummy_campus = await Campus.create({
+        name: "Hunter College",
+        description: "This is a school in NYC.",
+        address: "695 Park Ave, New York, NY 10065"
+    });
+    const dummy_campus2 = await Campus.create({
+        name: "Harvard",
+        description: "This is a school in MA.",
+        address: "Cambridge, MA"
+    });
 
-	const dummy_student = await Student.create({
-			firstname: "Joe",
-      lastname: "Shmo",
-		});
+    const dummy_student = await Student.create({
+        firstname: "Joe",
+        lastname: "Shmo",
+        email: "jshmo@crud.com",
+        gpa: 3.1
+    });
 
-	await dummy_student.setCampus(dummy_campus);
-	
+    await dummy_student.setCampus(dummy_campus);
+
 }
 
 module.exports = seedDB;
