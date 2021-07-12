dfa1c72f01c4069c4bca4c7085887f6f15830c56
vilnytskyy <vilnytskyy21@gmail.com>
2021-07-12 02:10:17 -0400

    Added email, imageUrl, and gpa info

--- database/models/Student.js
+++ database/models/Student.js
@@ -3,15 +3,43 @@ const db = require('../db');
 
 const Student = db.define("student", {
 
-  firstname: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
+    firstname: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: true
+        }
+    },
 
-  lastname: {
-    type: Sequelize.STRING,
-    allowNull: false
-  }
+    lastname: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: true
+        }
+    },
+
+    email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: true,
+            isEmail: true
+        }
+    },
+
+    imageUrl: {
+        type: Sequelize.STRING,
+        defaultValue: "student.svg"
+    },
+
+    gpa: {
+        type: Sequelize.DECIMAL,
+        validate: {
+            min: 0.0,
+            max: 4.0
+        }
+    },
 
 });
 
